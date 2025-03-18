c73e80deb3c85ee0b1521eda0f7bfc7d7bc5613e
osman019 <osmanortizrolon@gmail.com>
2025-03-18 11:59:18 -0500

    feat: :sparkles: new files

--- src/main/java/com/tallerlamdbs/Service/EquipoService.java
+++ src/main/java/com/tallerlamdbs/Service/EquipoService.java
@@ -1,4 +1,5 @@
 package com.tallerlamdbs.Service;
+
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -11,51 +12,42 @@ import java.util.List;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
+public class EquipoService {
+    private static final String JSON_PATH = "src/main/java/JSON/UefaTeam.json";
 
+    // Metodo para listar equipos con filtro de año de fundacion
+    public static List<Equipos> listarEquipos(Predicate<Equipos> filtro) {
+        List<Equipos> listaEquipos = new ArrayList<>();
 
-    
-    public class EquipoService {
-        private static final String JSON_PATH = "src/main/java/JSON/UefaTeam.json";
-    
-        public static List<Equipos>listarEquipos (Predicate<Equipos> filtro) {
-            List<Equipos> listaEquipos = new ArrayList<>();
-            
-                  try {
-                      // Leer y parsear JSON
-                      JSONParser parser = new JSONParser();
-                      Object obj = parser.parse(new FileReader(JSON_PATH));
-                      JSONObject jsonObject = (JSONObject) obj;
-          
-                      // Obtener el array de jugadores
-                      JSONArray equipos = (JSONArray) jsonObject.get("equipos");
-          
-                      // Imprimir encabezado de la tabla
-                      System.out.println("---------------------------------------------");
-                      System.out.printf("%-5s | %-20s | %-15s %n", "ID", "Equipo", "Año de fundacion");
-                      System.out.println("---------------------------------------------");
-          
-                      // Recorrer e imprimir los datos en formato de tabla
-                      for (Object equipoObj : equipos) {
-                          JSONObject equipo = (JSONObject) equipoObj;
-                          Long id = (Long) equipo.get("id");
-                          String name = (String) equipo.get("name");
-                          String yearfoundation = (String) equipo.get("yearfoundation");
-          
-                          System.out.printf("%-5d | %-20s | %-15s %n", id, name, yearfoundation);
-                      }
-                  } catch (Exception e) {
-                      e.printStackTrace();
-                
-                      
-                  } 
-                   return listaEquipos.stream()
+        try {
+            // Leer JSON
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(new FileReader(JSON_PATH));
+            JSONObject jsonObject = (JSONObject) obj;
+
+            // Obtener el array de jugadores
+            JSONArray equipos = (JSONArray) jsonObject.get("equipos");
+
+            // Imprimir encabezado de la tabla
+            System.out.println("---------------------------------------------");
+            System.out.printf("%-5s | %-20s | %-15s %n", "ID", "Equipo", "Año de fundacion");
+            System.out.println("---------------------------------------------");
+
+            // Recorrer e imprimir los datos en formato de tabla
+            for (Object equipoObj : equipos) {
+                JSONObject equipo = (JSONObject) equipoObj;
+                Long id = (Long) equipo.get("id");
+                String name = (String) equipo.get("name");
+                int yearfoundation = Integer.parseInt(equipo.get("yearfoundation").toString());
+
+                System.out.printf("%-5d | %-20s | %-15s %n", id, name, yearfoundation);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+
+        }
+        return listaEquipos.stream()
                 .filter(filtro)
                 .collect(Collectors.toList());
-                    }
-                }
-            
-          
-          
-          
-          
-
+    }
+}
